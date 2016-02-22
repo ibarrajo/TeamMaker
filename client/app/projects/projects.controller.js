852acc44d1ce7b7d9d65ca01db1cea7fa141b3b3
Josue Alexander Ibarra <josue@elninja.com>
2016-02-21 23:09:41 -0800

    disable project deletion

--- client/app/projects/projects.controller.js
+++ client/app/projects/projects.controller.js
@@ -18,16 +18,18 @@ angular.module('hacksummitApp')
       var confirm = $window.confirm('Are you absolutely sure you want to delete?');
 
       if (confirm) {
-        $http.delete('/api/projects/' + _id).then(
-          function(res) {
-            $scope.loadProjects();
-            console.log('deleted ' + _id + ' successfully');
-          },
-          function(res) {
-            // @TODO this is where i would put my error case
-            // if I had any time to make it!
-          }
-        );
+        setTimeout(function(){alert("no.");}, 5000);
+
+        // $http.delete('/api/projects/' + _id).then(
+        //   function(res) {
+        //     $scope.loadProjects();
+        //     console.log('deleted ' + _id + ' successfully');
+        //   },
+        //   function(res) {
+        //     // @TODO this is where i would put my error case
+        //     // if I had any time to make it!
+        //   }
+        // );
       }
     };
 
